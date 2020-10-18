2618abf027db3ade461410b786cfd6a3549a078c
Esref Oguzhan Yildirim <e.oguzhan.yildirim@gmail.com>
2020-10-18 20:05:31 +0300

    Urls updated

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,8 @@ from . import views
 from django.urls import include, path
 
 urlpatterns = [
-    path('update_tables', views.update_tables, name='index'),
+    path('index', views.index, name='index'),
+    path('update_tables', views.update_tables, name='update_tables'),
     path('get_dates', views.get_dates, name='get_dates'),
     path('get_postcodes', views.get_postcodes, name='get_postcodes'),
     path('get_avg_prices', views.get_avg_prices, name='get_avg_prices'),
